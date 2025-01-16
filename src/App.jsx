cf3be51c454a80a8e4b8dd45cc559d1eb23786c9
klabarre <klabarre@dawan.fr>
2025-01-16 13:48:56 +0100

    ajout images / ajout exemple gestio ndes routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<>HOMEPAGE</>} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <>ERREUR</>,
+      children: [
+        { path: "", element: <>Homepage</> },
+        { path: "/products", element: <>my product</> },
+        { path: "/*", element: <>404 NOT FOUND</> },
+      ],
+    },
+  ]);
+
+  function Root() {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    );
+  }
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<>HOMEPAGE</>} />
+//         <Route path="/products" element={<>PRODUCTS</>} />
+//         <Route path="*" element={<>404</>} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
